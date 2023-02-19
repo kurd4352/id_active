c99b3cd305d009418b81956fa7d6bf348da47dd5
kurd4352 <125754773+kurd4352@users.noreply.github.com>
2023-02-19 22:49:13 +0300

    addod_id

---
+++ addod.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import time
+import requests
+os.system('clear')
+os.system('rm -rf list.txt')
+os.system('AHMAD -u > list.txt')
+uid = open('list.txt', 'r')
+for j in uid:
+    sp = j.split()
+
+def chk():
+
+  uuid = str(os.geteuid()) + str(os.getlogin()) 
+
+  id = "|".join(uuid)
+
+  print("\n\n\x1b[37;1m  YOUR ID : \033[94m"+id) 
+
+  try: 
+
+    httpCaht = requests.get("://https://github.com/kurd4352/id_active/blob/main/itz_addod_active").text 
+
+    if id in httpCaht: 
+
+      print("\033[92m  YOUR ID IS ACTIVE.........\033[97m") 
+
+      msg = str(os.geteuid()) 
+
+      time.sleep(1) 
+
+      pass 
+
+    else: 
+
+      print("\033[0;96m Id kat active nya Tkaya bo Active krdn NAME bnera bo telegram @i4m_adod") 
+
+
+      time.sleep(1) 
+
+      sys.exit() 
+
+  except: 
+
+    sys.exit() 
+
+    if name == '__main__': 
+
+     print (logo)
+
+     chk() 
+
+     
+
+chk() 
